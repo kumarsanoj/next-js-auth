0039715e01b0ebd82d2e9195bf65211696091918
sanoj kumar <sanoj.kumar@iqvia.com>
2023-02-03 17:26:09 +0530

    testing logflare

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import logger from "./logger/logger";
 
+export const config = {
+  matcher: ["/", "/home"],
+};
+
 export function middleware(req) {
   let { pathname } = req.nextUrl;
   logger.info({ event: "middleware" }, "navigating to %s", pathname);
