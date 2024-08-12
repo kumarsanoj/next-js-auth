495cd31e5f2be7bea4ece9bb156560b83cf4117c
kumarsanoj <kumarsanoj2101@gmail.com>
2024-08-12 12:52:08 +0530

    Update getIp.js

--- pages/api/getIp.js
+++ pages/api/getIp.js
@@ -2,6 +2,8 @@ export default function handler(req, res) {
     // Check for the X-Forwarded-For header
     let ip = req.ip ? req.ip : req.headers['x-real-ip']
     const forwardedFor = req.headers['x-forwarded-for'];
+    console.log('IP:::::::', ip);
+    console.log('forwardedFor:::::::', forwardedFor);
     if (!ip && forwardedFor) { 
         const ipAddress = forwardedFor ? forwardedFor.split(',')[0] : req.connection.remoteAddress;
         ip = forwardedFor ? forwardedFor : 'unknown'
