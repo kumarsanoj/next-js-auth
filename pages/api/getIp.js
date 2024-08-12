8b675268a9e1f55e2a03948bfafa615612424de2
kumarsanoj <kumarsanoj2101@gmail.com>
2024-08-12 12:50:44 +0530

    Update and rename hello-pino.js to getIp.js

---
+++ pages/api/getIp.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+    // Check for the X-Forwarded-For header
+    let ip = req.ip ? req.ip : req.headers['x-real-ip']
+    const forwardedFor = req.headers['x-forwarded-for'];
+    if (!ip && forwardedFor) { 
+        const ipAddress = forwardedFor ? forwardedFor.split(',')[0] : req.connection.remoteAddress;
+        ip = forwardedFor ? forwardedFor : 'unknown'
+ 
+    }
+    
+    res.status(200).json({ ip: ip });
+  }
